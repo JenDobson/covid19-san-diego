78f033757902bc35b00d421a47162859b8de0bc6
Jen Dobson <jendobson@gmail.com>
2020-06-09 07:23:34 -0700

    move files

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-source //Users/jen/projects/covid-san-diego/env/covidsd/bin/activate
 cd /Users/jen/projects/covid-san-diego
 git pull
 
-python -c 'import data_scraper.parser as p; p.get_pdfs()'
-python -c 'import data_scraper.parser as p; p.get_daily_status()'
-python -c 'import data_scraper.parser as p; p.get_data_by_city()'
-nozc=$(python -c 'import data_scraper.parser as p; p.get_data_by_zipcode()')
+#python -c 'import data_scraper.parser as p; p.get_pdfs()'
+#python -c 'import data_scraper.parser as p; p.get_daily_status()'
+#python -c 'import data_scraper.parser as p; p.get_data_by_city()'
+#nozc=$(python -c 'import data_scraper.parser as p; p.get_data_by_zipcode()')
+/Users/jen/projects/covid-san-diego/env/covidsd/bin/python /Users/jen/projects/covid-san-diego/get_data.py
+
+
 
 d=$(date +%Y-%m-%d)
+echo "Update data for $d"
+
 t=$(date +%Y%m%dT%H:%M:%S)
 echo "Update data for $t" >> scripts/log.txt
 
